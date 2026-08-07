cee23d87f29ca245941e11ac60fb72dc3d825623
r84134530-cyber <r84134530@gmail.com>
2026-08-07 23:22:28 +0300

    Initialize basic HTTP server in server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Botul de Faction Warn ruleaza!\n');
+});
+server.listen(process.env.PORT || 3000);
+
